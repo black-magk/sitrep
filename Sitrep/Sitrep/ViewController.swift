0900b56c1ac733b54b6141f49e916c76529b9587
Landon Carr <landon.c.carr@gmail.com>
2018-08-21 17:51:24 -0700

    Sitrep First Commit

---
+++ Sitrep/Sitrep/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sitrep
+//
+//  Created by Landon Carr on 8/21/18.
+//  Copyright © 2018 Landon Carr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
